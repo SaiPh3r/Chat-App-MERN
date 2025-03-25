333da5e2a4c0d1ae75ece59a8c33d2452eb26af3
SaiPh3r <saianand750@gmail.com>
2025-03-25 13:30:36 +0530

    conversations hook logics

---
+++ frontend/src/hooks/useGetConversations.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react'
+import toast from 'react-hot-toast'
+
+const useGetConversations = () => {
+    const[loader,setLoader] = useState(false)
+    const[conversations,setConversations] = useState([])
+
+    useEffect(() => {
+        const getConversations = async()=>{
+            setLoader(true)
+            try {
+                const res = await fetch('/api/users')
+                const data = res.json()
+                if(data.error){
+                    throw new Error(data.error)
+
+                }
+                setConversations(data)
+                
+            } catch (error) {
+                toast.error(error.message)
+                
+            }
+            finally{
+                setLoader(false)
+            }
+        }
+        getConversations()
+    }, [])
+    return {loader,conversations}
+}
+
+export default useGetConversations
